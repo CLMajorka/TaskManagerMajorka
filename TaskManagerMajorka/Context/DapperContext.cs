a1e343aaff10df76b8145044a3d83ce15a0ace14
dariusz999 <dariusz.kanclerz@interia.pl>
2024-04-03 20:20:04 +0200

    ADD DapperContext

---
+++ TaskManagerMajorka/Context/DapperContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace TaskManagerMajorka.Context
+{
+    public class DapperContext : IDapperContext
+    {
+        private readonly IConfiguration _configuration;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IDbConnection CreateConnection()
+            => new
+                SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+
+ 
+    }
+}
