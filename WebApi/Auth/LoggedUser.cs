7a05b417cfa7865bca3f172814fdb3ede9ee283b
Sebieski <sebuuunio@gmail.com>
2024-04-05 22:51:25 +0200

    ﻿MOD WebApi

--- WebApi/Auth/LoggedUser.cs
+++ WebApi/Auth/LoggedUser.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.Identity.Client;
+using Domain.Models;
 
 namespace WebApi.Auth
 {
     public class LoggedUser : IUser
     {
-
+        public int UserId { get; }
+        public string UserName { get; }
+        public string Email { get; }
+        public UserRole Role { get; }
     }
 }
