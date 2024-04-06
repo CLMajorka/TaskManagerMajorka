0da701b04b2389ebbf79bdeced0e3c30177722d5
Sebieski <sebuuunio@gmail.com>
2024-04-06 12:59:16 +0200

    ADD CommentDto

---
+++ Application/DTOs/CommentDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class CommentDto
+    {
+        public string Text { get; set; }
+        public int TaskId { get; set; }
+        public int LoggedUserId { get; set; }
+
+        public CommentDto(string text, int taskId, int loggedUserId)
+        {
+            Text = text;
+            TaskId = taskId;
+            LoggedUserId = loggedUserId;
+        }
+    }
+}
