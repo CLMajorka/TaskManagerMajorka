fb6354a931b27811afaf8ccf40bcbc81187b4660
Sebieski <sebuuunio@gmail.com>
2024-04-06 10:32:16 +0200

    FIX CommentDto

---
+++ Application/DTOs/CommentDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class CommentDto
+    {
+        public string Text { get; private set; }
+        public int? TaskId { get; set; }
+        public int? UserId { get; set; }
+
+        public CommentDto(string text)
+        {
+            Text = text;
+        }
+    }
+}
