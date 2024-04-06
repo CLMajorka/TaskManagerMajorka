e2daba6c015b1b6516bc375e31a1d8045459c93e
Sebieski <sebuuunio@gmail.com>
2024-04-06 15:18:07 +0200

    ADD CommentRepository & Interface

---
+++ Persistence/Repository/CommentRepository.cs
@@ -0,0 +1,49 @@
+﻿using Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Domain.Models;
+
+namespace Persistence.Repository
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly IDapperContext _context;
+
+        public CommentRepository(IDapperContext dapperContext) => _context = dapperContext;
+
+
+        public Comment Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Comment> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Create(Comment model)
+        {
+            var sql = "INSERT INTO Comments (TaskId, Text, CreatedBy, CreatedAt, ModifiedBy, ModifiedAt) VALUES (@TaskId, @Text, @CreatedBy, @CreatedAt, @ModifiedBy, @ModifiedAt); SELECT CAST(SCOPE_IDENTITY() AS int)";
+            using (var connection = _context.CreateConnection())
+            {
+                var id = connection.QuerySingle<int>(sql, model);
+                return id;
+            }
+        }
+
+        public bool Update(Comment model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Persistence/Repository/ICommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Persistence.Repository
+{
+    public interface ICommentRepository : IGenericRepository<Comment>
+    {
+        Comment Get(int id);
+        IEnumerable<Comment> GetAll();
+        int Create(Comment model);
+        bool Update(Comment model);
+        bool Delete(int id);
+    }
+}
